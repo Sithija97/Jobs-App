6025882b18b4039d4ee7638e56aff42d7a53e9eb
Sithija Shehara <nsithijashehara@gmail.com>
2021-11-08 23:08:20 +0530

    job card structure

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import React from "react";
 import Header from "./header";
+import JobCard from "./jobCard";
 import SearchBar from "./searchBar";
 
 const Index = () => {
@@ -10,6 +11,7 @@ const Index = () => {
       <Grid container justifyContent="center">
         <Grid item xs={10}>
           <SearchBar />
+          <JobCard />
         </Grid>
       </Grid>
     </React.Fragment>
--- src/components/jobCard.jsx
+++ src/components/jobCard.jsx
@@ -1,7 +1,29 @@
+import { Button, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 
 const JobCard = () => {
-  return <div></div>;
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs>
+          <Typography>Frontend Developer</Typography>
+          <Typography>Google</Typography>
+        </Grid>
+        <Grid item container xs>
+          <Grid item>React.js</Grid>
+          <Grid item>Node.js</Grid>
+          <Grid item>MongoDB</Grid>
+        </Grid>
+        <Grid item xs>
+          <Typography>20 min ago | Full time | Remote</Typography>
+        </Grid>
+        <Grid item xs>
+          <Button variant="contained">Check</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default JobCard;
